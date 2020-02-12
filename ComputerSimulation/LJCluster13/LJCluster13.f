90ca69ed267a47be50a06898b95672f9e659da55
Alex Gunasekera <alexgunasekera@Installs-MacBook-Pro.local>
2020-02-12 10:35:37 +0000

    Moved functions to subroutines

--- ComputerSimulation/LJCluster13/LJCluster13.f
+++ ComputerSimulation/LJCluster13/LJCluster13.f
@@ -1,74 +1,11 @@
-      FUNCTION DISP(XI, XJ)
-      REAL*8 :: XI(3), XJ(3), DISP(3)
-*
-* Displacement between two vectors in 3D space
-*
-      INTEGER :: N
-
-      DO 10 N = 1, 3
-          DISP(I) = XJ(N) - XI(N)
-10    CONTINUE
-
-      RETURN
-      END FUNCTION
-
-      FUNCTION MAG(VEC)
-*
-* Magnitude of a given 3D vector
-*
-      REAL*8 :: VEC(3), MAG
-      INTEGER :: N
-
-      MAG = 0.
-
-      DO 20 N = 1, 3
-          MAG = MAG + VEC(I) ** 2
-20    CONTINUE
-
-      RETURN
-      END FUNCTION
-
-      FUNCTION LJ2FORCE(R)
-      REAL*8 :: R, LJ2FORCE
-*
-* Calculate 2-particle Lennard-Jones force divided by displacement vector
-*
-      REAL*8 :: RTOMIN6
-
-      RTOMIN6 = R ** (-6)
-      LJ2FORCE = 24 * (RTOMIN6 - 2 * RTOMIN6 ** 2) / (R ** 2)
-
-      RETURN
-      END FUNCTION
-
-      FUNCTION LJFIJ(XI, XJ)
-      REAL*8 :: XI(3), XJ(3), LJFIJ(3)
-*
-* Lennard-Jones force of particle j on particle i
-*
-      REAL*8 :: DISPIJ(3), DISTIJ, FSCAL
-      INTEGER :: N
-
-      DISPIJ = DISP(XI, XJ)
-      DISTIJ = MAG(DISPIJ)
-      FSCAL = LJ2FORCE(DISTIJ)
-
-      DO 90 N = 1, 3
-          LJFIJ(N) = DISPIJ(N) * FSCAL
-90    CONTINUE
-
-      RETURN
-      END FUNCTION
-
-
       PROGRAM LJ13CLUSTER
 *
 *
 *
       IMPLICIT NONE
       INTEGER :: NATOMS, I, NSTEPS
-      REAL*8 :: XMATRIX(3, *), VMATRIX(3, *), FMATRIX(3, *), 
-     &NEWFMATRIX(3, *), DT
+      REAL*8 :: XMATRIX(3, 13), VMATRIX(3, 13), FMATRIX(3, 13), 
+     &NEWFMATRIX(3, 13), DT
       REAL*8 :: V_VERLET_X(3), V_VERLET_V(3), FORCE(3)
 
       NATOMS = 13
@@ -109,6 +46,68 @@
 
       END SUBROUTINE
 
+      SUBROUTINE GETDISP(XI, XJ, DISP)
+      REAL*8 :: XI(3), XJ(3), DISP(3)
+*
+* Displacement between two vectors in 3D space
+*
+      INTEGER :: N
+
+      DO 10 N = 1, 3
+          DISP(I) = XJ(N) - XI(N)
+10    CONTINUE
+
+      RETURN
+      END SUBROUTINE
+
+      SUBROUTINE GETSQUAREMAGNITUDE(VEC, MAG2)
+*
+* Magnitude of a given 3D vector
+*
+      REAL*8 :: VEC(3), MAG2
+      INTEGER :: N
+
+      MAG2 = 0
+
+      DO 20 N = 1, 3
+          MAG2 = MAG2 + VEC(I) ** 2
+20    CONTINUE
+
+      RETURN
+      END SUBROUTINE
+
+      SUBROUTINE GETFORCESCALAR(R2, LJ2FSCAL)
+      REAL*8 :: R2, LJ2FSCAL
+*
+* Calculate 2-particle Lennard-Jones force divided by displacement vector
+*
+      REAL*8 :: RTOMIN6
+
+      RTOMIN6 = R2 ** (-3)
+      LJ2FSCAL = 24 * (RTOMIN6 - 2 * RTOMIN6 ** 2) / R2
+
+      RETURN
+      END SUBROUTINE
+
+      SUBROUTINE GETLJFORCEIJ(XI, XJ, LJFIJ)
+      REAL*8 :: XI(3), XJ(3), LJFIJ(3)
+*
+* Lennard-Jones force of particle j on particle i
+*
+      REAL*8 :: DISPIJ(3), DISTIJ2, FSCAL
+      INTEGER :: N
+
+      CALL GETDISP(XI, XJ, DISPIJ)
+      CALL GETSQUAREMAGNITUDE(DISPIJ, DISTIJ2)
+      CALL GETFORCESCALAR(DISTIJ2, FSCAL)
+
+      DO 90 N = 1, 3
+          LJFIJ(N) = DISPIJ(N) * FSCAL
+90    CONTINUE
+
+      RETURN
+      END SUBROUTINE
+
       SUBROUTINE V_VERLET_UPDATE_XMATRIX
      &(XMATRIX, VMATRIX, FMATRIX, NATOMS, DT)
       REAL*8 :: XMATRIX(3, *), VMATRIX(3, *), FMATRIX(3, *), DT
@@ -139,7 +138,7 @@
 
       DO 70 I = 1, NATOMS
           DO 80 N = 1, 3
-              VMATRIX(N) = VMATRIX(N, I) + (FMATRIX(N, I) 
+              VMATRIX(N, I) = VMATRIX(N, I) + (FMATRIX(N, I) 
      &+ NEWFMATRIX(N, I)) * DT / 2
 80        CONTINUE
 70    CONTINUE
@@ -162,7 +161,7 @@
           XI = XMATRIX(:, I)
           DO 110 J = I + 1, NATOMS
               XJ = XMATRIX(:, J)
-              FIJ = LJFIJ(XI, XJ)
+              CALL GETLJFORCEIJ(XI, XJ, FIJ)
               DO 120 N = 1, 3
                   FMATRIX(N, I) = FMATRIX(N, I) + FIJ(N)
                   FMATRIX(N, J) = FMATRIX(N, J) + FIJ(N)
@@ -183,10 +182,11 @@
 
       OPEN(1, FILE = "initialconfig.txt")
 
-      READ(1, *) XMATRIX
+*      READ (1, *) XMATRIX
 
       CLOSE(1)
 
+      CALL SETMATRIXZERO(XMATRIX, NATOMS)
       CALL SETMATRIXZERO(VMATRIX, NATOMS)
 
       END SUBROUTINE
