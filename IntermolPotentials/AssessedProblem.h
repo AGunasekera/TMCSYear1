08651a17b450a25dfa2c75705bdc51c48324fed1
Alex Gunasekera <alexander.gunasekera@gmail.com>
2020-06-15 01:29:11 +0100

    working iterative procedure for updating dipoles

--- IntermolPotentials/AssessedProblem.h
+++ IntermolPotentials/AssessedProblem.h
@@ -4,18 +4,36 @@
 // Class for a point charge at a given position with a given charge.
 class pointCharge {
     double position;
+    double potential;
+    double field;
     double charge;
     public:
         pointCharge(){
             position = 0;
+            potential = 0;
+            field = 0;
             charge = 0;
         }
-        void setCharge(double position_, double charge_){
+        void setPosition(double position_){
             position = position_;
+        }
+        void setPotential(double potential_){
+            potential = potential_;
+        }
+        void setField(double field_){
+            field = field_;
+        }
+        void setCharge(double charge_){
             charge = charge_;
         }
         double getPosition(){
             return position;
+        }
+        double getPotential(){
+            return potential;
+        }
+        double getField(){
+            return field;
         }
         double getCharge(){
             return charge;
@@ -25,24 +43,41 @@ class pointCharge {
 // Class for a dipole at a given position, with a given polarisability, and a corresponding dipole moment.
 class dipole {
     double position;
+    double potential;
+    double field;
     double polarisability;
     double moment;
     public:
         dipole(){
             position = 0;
+            potential = 0;
+            field = 0;
             polarisability = 0;
             moment = 0;
         }
-        void setDipole(double position_, double polarisability_, double moment_){
+        void setPosition(double position_){
             position = position_;
+        }
+        void setPotential(double potential_){
+            potential = potential_;
+        }
+        void setField(double field_){
+            field = field_;
+        }
+        void setPolarisability(double polarisability_){
             polarisability = polarisability_;
-            moment = moment_;
         }
         void setMoment(double moment_){
             moment = moment_;
         }
         double getPosition(){
             return position;
+        }
+        double getPotential(){
+            return potential;
+        }
+        double getField(){
+            return field;
         }
         double getPolarisability(){
             return polarisability;
