1d704c016e58d7b2583e005f63138cd4d281bd84
Alex Gunasekera <alexander.gunasekera@gmail.com>
2020-06-19 05:10:15 +0100

    Fix iteration; use command line arguments

--- StatMech/IsingRGIterativeMap.cpp
+++ StatMech/IsingRGIterativeMap.cpp
@@ -1,32 +1,6 @@
 #include <iostream>
 #include <string>
-
-double f(double, double), g(double, double);
-
-int main() {
-    double X, Y;
-    double newX, newY;
-    std::cout << "Initial X: ";
-    std::cin >> X;
-    std::cout << "Initial Y: ";
-    std::cin >> Y;
-    std::string flag;
-    do {
-        std::cout << "Iterate (X, Y) => (X', Y')?";
-        getline(std::cin, flag);
-        if (flag.empty()) {
-            newX = f(X, Y);
-            newY = g(X, Y);
-            X = newX;
-            Y = newY;
-            std::cout << X << "    " << Y << "\n";
-        }
-        else {
-            break;
-        }
-    } while (true);
-    return 0;
-}
+#include <cmath>
 
 double f(double X, double Y){
     return X * (1 + Y) * (1 + Y) / ((X + Y) * (1 + X * Y));
@@ -36,3 +10,27 @@ double g(double X, double Y){
     return Y * (X + Y) / (1 + X * Y);
 }
 
+int main(int argc, char *argv[]) {
+    double X = 0., Y = 0.;
+    double newX = 1., newY = 1.;
+
+    for (int i=1; i<argc; i+=2 ){
+        std::string flag = std::string(argv[i]);
+        std::string value = std::string(argv[i+1]);
+        if (flag == "-x"){
+            newX = stod(value);
+        } else if (flag == "-y"){
+            newY = stod(value);
+        }
+    }
+
+    std::cout << newX << "    " << newY << "\n";
+    while ((std::abs(newX-X) > pow(10.,-6.)) || (std::abs(newY-Y) > pow(10.,-6.))) {
+        X = newX;
+        Y = newY;
+        newX = f(X, Y);
+        newY = g(X, Y);
+        std::cout << newX << "    " << newY << "\n";
+    }
+    return 0;
+}
